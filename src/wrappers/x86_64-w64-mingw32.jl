b98461e3592e0fe2c57b986d74798c5f7e159fa9
jlbuild <juliabuildbot@gmail.com>
2020-10-16 19:01:00 +0000

    Zlib_jll build 1.2.11+18

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -7,7 +7,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libz,
-        "bin/libz.dll",
+        "bin\\libz.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
